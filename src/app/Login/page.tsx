4c3ea304c20052fd1e8b98b574215de9355425f5
SithumDuleka <sithumdulekakalhara@gmail.com>
2024-12-27 13:18:40 +0530

    Forgot password added

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Login() {
   return (
     <div>
       Welcome to login page
+      <Link href="/Login/ForgotPassword">Users</Link>
     </div>
   )
 }
